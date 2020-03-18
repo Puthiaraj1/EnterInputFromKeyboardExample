3c792b9b817da9e1da437c551b037a9ab7cb4da2
Puthiaraj Anbalagan <puthiarajanbalagan@Puthiarajs-MacBook-Pro.local>
2020-03-18 17:04:15 -0400

    Initial commit

---
+++ InputFromKeyboard_SplitString.py
@@ -0,0 +1,15 @@
+# greeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + ' '+ name)
+
+
+splitStringByNewLine = "This string has been\nsplit over\nserveral\nlines"
+print(splitStringByNewLine)
+
+tabbedString = "1\t2\t3\t4\t5\t"
+print(tabbedString)
+
+splitStringByNewLineUsingTripleQuotes = """This String has been
+split over 
+serveral Lines"""
+print(splitStringByNewLineUsingTripleQuotes)
